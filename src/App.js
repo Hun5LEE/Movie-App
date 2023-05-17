8f4973517326afb6c58c953cf0169d7a6a8b9bb4
Hun5LEE <jhlee091697@gmail.com>
2023-05-18 00:20:07 +0900

    코인목록 나열

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [loading, setLoading] = useState(ture);
+  const [loading, setLoading] = useState(true);
+  const [coinData, setCoinData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          "https://api.coinpaprika.com/v1/tickers"
+        );
+        setCoinData(data);
+        setLoading(false);
+      } catch (err) {
+        alert("Error loading");
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
       <h1>The coins!</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coinData.map((a, i) => {
+            return (
+              <li key={i}>
+                {a.name} ({a.symbol} : ${a.quotes.USD.price})
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
