44c5d5dc441d698c3ee93947cde9d61e78028d12
Hun5LEE <jhlee091697@gmail.com>
2023-05-19 16:00:47 +0900

    홈페이지 스타일 적용

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,84 @@
 import { Link } from "react-router-dom";
+import { styled } from "styled-components";
 import PropTypes from "prop-types";
 
 export default function Movie({ movie }) {
-  const { id, medium_cover_image, title, summary, genres } = movie;
+  const { id, medium_cover_image, title, summary, genres, year } = movie;
+
+  const Movie = styled.div`
+    background-color: white;
+    margin-bottom: 70px;
+    font-weight: 300;
+    padding: 20px;
+    border-radius: 5px;
+    color: #adaeb9;
+    display: grid;
+    grid-template-columns: minmax(150px, 1fr) 2fr;
+    grid-gap: 20px;
+    text-decoration: none;
+    color: inherit;
+    box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
+      0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+  `;
+
+  const MovieImg = styled.img`
+    position: relative;
+    top: -50px;
+    max-width: 150px;
+    width: 100%;
+    margin-right: 30px;
+    box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
+      0 18px 36px -18px rgba(0, 0, 0, 0.3),
+      0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+  `;
+
+  const MovieTitle = styled.h2`
+    margin: 0;
+    font-weight: 300;
+    text-decoration: none;
+    a {
+      margin-bottom: 5px;
+      font-size: 24px;
+      color: #2c2c2c;
+      text-decoration: none;
+    }
+  `;
+
+  const MovieYear = styled.h3`
+    margin: 0;
+    font-weight: 300;
+    text-decoration: none;
+    margin-right: 10px;
+    font-size: 14px;
+  `;
+
+  const MovieGenres = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 5px 0px;
+    li {
+      margin-right: 10px;
+      font-size: 14px;
+    }
+  `;
 
   return (
-    <div>
-      <Link to={`/movie/${id}`}>
-        <img src={medium_cover_image} alt="..." />
-        <h2>{title}</h2>
-      </Link>
-      <p>{summary}</p>
+    <Movie>
+      <MovieImg src={medium_cover_image} alt="..." />
+      <MovieTitle>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </MovieTitle>
+      <MovieYear>{year}</MovieYear>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres?.map((genre, i) => {
           return <li key={genre}>{genre}</li>;
         })}
       </ul>
-    </div>
+    </Movie>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,39 @@
 import Movie from "../components/Movie";
 import { useEffect, useState } from "react";
+import { styled } from "styled-components";
 import axios from "axios";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+
+  const Loader = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 300;
+  `;
+
+  const Container = styled.div`
+    height: 100%;
+    display: flex;
+    justify-content: center;
+  `;
+
+  const Movies = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, minmax(400px, 1fr));
+    grid-gap: 100px;
+    padding: 50px;
+    width: 80%;
+    padding-top: 70px;
+    @media screen and (max-width: 1090px) {
+      grid-template-columns: 1fr;
+      width: 100%;
+    }
+  `;
 
   // GET해온 data의 구조 -> data > data > movies
   useEffect(() => {
@@ -15,27 +44,31 @@ export default function Home() {
         } = await axios.get(
           "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
         );
-        console.log(data);
         setMovies(data.movies);
         setLoading(false);
       } catch (err) {
+        console.log(err);
         alert("Error loading");
       }
     })();
   }, []);
 
   return (
-    <div className="App">
-      <h1>Movies Info</h1>
+    <Container>
       {loading ? (
-        <h1>Loading...</h1>
+        <>
+          <h1>Loading...</h1>
+          <Loader>
+            <span>Loading...</span>
+          </Loader>
+        </>
       ) : (
-        <div>
+        <Movies>
           {movies?.map((movie) => {
             return <Movie movie={movie} key={movie.id} />;
           })}
-        </div>
+        </Movies>
       )}
-    </div>
+    </Container>
   );
 }
